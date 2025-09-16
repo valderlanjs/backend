098e5cf5e44cd2e38cbdc1e008043e5368749b58
Valderlan Silva <valderlanjosr15@gmail.com>
2025-09-16 15:12:15 -0300

    realizado implementação dos cards grandes

--- routes/CardRoute.js
+++ routes/CardRoute.js
@@ -1,6 +1,6 @@
 // routes/cardRoutes.js
 import express from "express";
-import { createCard, getCards } from "../controllers/cardController.js";
+import { createCard, getCards } from "../controllers/CardController.js";
 
 const cardRoute = express.Router();
 
